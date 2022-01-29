af18edec11408587013d9f0bdff8e94b6eb72000
gerarduxi <gerardosusa@hotmail.com>
2022-01-29 14:26:15 -0500

    clases modificadas

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,11 +1,17 @@
 package test;
 
 public class Main extends Test{
+
     //static Test classTest = new Test();
+
     public static void main(String args[]){
+
        // classTest.newTest();
+
         newTest();
+
         newTest2();
+
         newTest3();
 
     }
--- src/test/Test.java
+++ src/test/Test.java
@@ -5,9 +5,11 @@ public class Test {
      static void newTest(){
         System.out.println("imprimiendo brock class Test");
     }
-    protected static void newTest2(){
+
+     protected static void newTest2(){
         System.out.println("imprimiendo brock class 2Test");
     }
+
      static void newTest3(){
         System.out.println("imprimiendo brock class 3Test");
     }
