031620a7bc308c5f3b4e4278cdeea49e67cf5c56
gerarduxi <gerardosusa@hotmail.com>
2022-01-29 14:14:35 -0500

    estoy subiendo cambios de herencia de la clase Test

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+     static void newTest(){
+        System.out.println("imprimiendo brock class Test");
+    }
+    protected static void newTest2(){
+        System.out.println("imprimiendo brock class 2Test");
+    }
+     static void newTest3(){
+        System.out.println("imprimiendo brock class 3Test");
+    }
+
+}
